4bf1725b5012333e21ebe58a42c8f14cf5523f2c
SREENATH S <sreenaths.23aid@kongu.edu>
2025-04-18 14:00:32 +0530

    Time: 0 ms (100%), Space: 40.6 MB (51.31%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,5 +1,13 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        return Integer.bitCount(x^y);
+        int c=0;
+        int n=x^y;
+        while(n>0){
+            if((n & 1) ==1) {
+                c++;
+            }
+            n>>=1;
+        }
+        return c;
     }
 }
