02f7dc33894fa1c6c547bbe5ea14ce3666a4d92b
SREENATH S <sreenaths.23aid@kongu.edu>
2025-04-19 11:25:39 +0530

    Time: 1 ms (99.9%), Space: 46.4 MB (35.48%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
        int n=0;
-       for(int num : nums){
-        n=n^num;
+       for(int i=0;i<nums.length;i++){
+        n=n^nums[i];
        }
        return n;
     }
