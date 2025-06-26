581ac777d7c7ff349e0e8dfc74837cc11301efe9
SREENATH S <sreenathsenthilkumar001@gmail.com>
2025-06-26 21:34:37 +0530

    Time: 5 ms (68.55%), Space: 44.3 MB (27.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0){
+        if(x<0){
             return false;
         }
         int rev=0;
-        int tem=x;
-        while(tem!=0){
-            int dig=(int) (tem % 10);
-            rev=rev*10+dig;
-            tem/=10;
+        int temp=x;
+        while(temp!=0){
+           int rem=temp%10;
+           rev=rev*10+rem;
+           temp/=10;
         }
-        return (rev == x);
+        return (rev==x);
     }
 }
