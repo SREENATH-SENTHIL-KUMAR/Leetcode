0b81271f62ce3f8362527affdc71d00b38c45feb
SREENATH S <sreenathsenthilkumar001@gmail.com>
2025-07-04 09:29:19 +0530

    Time: 1 ms (86.67%), Space: 40.7 MB (83.86%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)(Math.sqrt(x)); 
-    }
+        //return (int)(Math.sqrt(x));
+        long n=x;
+        while(n*n>x){
+            n=(n+x/n)/2;
+        }
+        return (int)(n);    }
 }
